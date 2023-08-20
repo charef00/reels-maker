213dcf305ba50bbfe26a5134b275d5c5eca64e3e
ayoub <ayouubcharef@gmail.com>
2023-08-20 12:02:31 +0100

    add sqlite3

--- project/settings.py
+++ project/settings.py
@@ -82,16 +82,20 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.mysql',  
+    #    'NAME': 'django',  
+    #    'USER': 'root',  
+    #    'PASSWORD': '',  
+    #    'HOST': '127.0.0.1',  
+    #    'PORT': '3306',  
+    #    'OPTIONS': {  
+    #        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+    #    }  
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'django',  
-        'USER': 'root',  
-        'PASSWORD': '',  
-        'HOST': '127.0.0.1',  
-        'PORT': '3306',  
-        'OPTIONS': {  
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-        }  
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
